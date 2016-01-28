4b1bdaf1f9d8b00feb8d8b45392bf591261eeb5d
Alfred Feldmeyer <alfred.feldmeyer@webtaurus.de>
2016-01-28 13:51:57 +0100

    ng-translate should now load

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from 'angular2/core';
-
+import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1>',
+    pipes: [TranslatePipe]
 })
 export class AppComponent { }
